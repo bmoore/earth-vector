954c902f4f4c8cdc4fb67c75efd257adfba06e5e
Brian Moore <moore.brian.d@gmail.com>
2016-04-20 23:25:41 -0400

    Add animations for the other drifters.

--- animation.py
+++ animation.py
@@ -20,19 +20,36 @@ map.drawparallels(np.arange(-90, 90, 30))
 map.readshapefile('./n130', 'n130')
 map.fillcontinents(color='#cccccc',lake_color='#ffffff')
 
-runtext = ax.text(1, 50, '')
-
 points = []
 with open('drifter.csv', 'rt') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in reader:
         try:
-            if row[2] and row[3] == 'LB54':
+            if row[2] and row[3] == 'LB52':
                 lat, lon = float(row[2]), float(row[1])
                 if lat > 10 and lon < 10:
                     points.append((float(row[2]), float(row[1]), row[3], row[4]))
         except ValueError as e:
             print(e)
+
+other_points = []
+river = []
+lb = []
+ub = []
+with open('pasta.csv', 'rt') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in reader:
+        try:
+            if row[0]:
+                river.append((float(row[0]), float(row[1])))
+            if row[3]:
+                lb.append((float(row[3]), float(row[4])))
+            if row[6]:
+                ub.append((float(row[6]), float(row[7])))
+        except ValueError as e:
+            print(e)
+
+other_points = river + lb + ub
 
 patches = []
 for info, shape in zip(map.n130_info, map.n130):
@@ -42,35 +59,58 @@ ax.add_collection(PatchCollection(patches, facecolor= '#ffffff', edgecolor='#fff
 x,y = map(0, 0)
 line = map.plot([], [], 'r', lw=2, zorder=5)[0]
 point = map.plot(x, y, 'bo', markersize=5, zorder=10)[0]
+font = {'family': 'sans-serif',
+        'color': 'black',
+        'weight': 'bold',
+        'size': 24,
+        }
+runtext = ax.text(4000, 2000, '', fontdict=font)
+
+start = map.plot(x, y, 'bo', markersize=5, zorder=10)[0]
+end = map.plot(x, y, 'bo', markersize=5, zorder=10)[0]
 
 def init():
     point.set_data([], [])
     line.set_data([], [])
     return line, point,
 
-# animation function.  This is called sequentially
 xs, ys = [], []
 runs = []
+# This animation function is for the drifter.csv tracks
+#def animate(i):
+#    lons = float(points[i][1])
+#    lats = float(points[i][0])
+#    x, y = map(lons, lats)
+#    if (points[i][2] not in runs):
+#        runs[:] = []
+#        runs.append(points[i][2])
+#        xs[:] = ys[:] = []
+#    xs.append(x)
+#    ys.append(y)
+#    if (i > 0):
+#        line.set_data(xs, ys)
+#    point.set_data(x, y)
+#    runtext.set_text(points[i][2])
+#    return (runtext,line,point)
+
+# This animation function is for the pasta.csv tracks
 def animate(i):
-    lons = float(points[i][1])
-    lats = float(points[i][0])
+    lons = float(river[i][1])
+    lats = float(river[i][0])
     x, y = map(lons, lats)
-    if (points[i][2] not in runs):
-        runs[:] = []
-        runs.append(points[i][2])
+    if not i:
         xs[:] = ys[:] = []
     xs.append(x)
     ys.append(y)
     if (i > 0):
         line.set_data(xs, ys)
     point.set_data(x, y)
-    runtext.set_text(points[i][3])
+    runtext.set_text('River')
     return (runtext,line,point)
 
 # call the animator.  blit=True means only re-draw the parts that have changed.
 anim = animation.FuncAnimation(plt.gcf(), animate, init_func=init,
-                               frames=len(points), interval=50, blit=True)
+                               frames=len(river), interval=50, blit=True)
 
 anim.save('animation.gif',writer='imagemagick',fps=30)
-
 #plt.show()
